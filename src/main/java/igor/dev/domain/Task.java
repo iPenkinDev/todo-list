5e9e4b2ac799092efe99a16531cb5800b1449e50
Igor Penkin <igor.penkin.dev@gmail.com>
2023-05-07 18:15:17 +0300

    add idea to gitignore

--- src/main/java/igor/dev/domain/Task.java
+++ src/main/java/igor/dev/domain/Task.java
@@ -1,2 +1,28 @@
-package igor.dev.domain;public class Task {
+package igor.dev.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@SuppressWarnings({"JpaDataSourceORMInspection", "com.haulmont.jpb.LombokDataInspection"})
+@Component
+@Entity
+@Table(name = "task")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.ORDINAL)
+    private Status status;
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>ToDo List</h2>
 </body>
 </html>
