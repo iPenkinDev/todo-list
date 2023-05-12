56efbe92b8e5c5a0f250cafd9f70a289dab830b2
Igor Penkin <igor.penkin.dev@gmail.com>
2023-05-12 16:29:18 +0300

    ready project

--- src/main/java/igor/dev/config/SpringConfig.java
+++ src/main/java/igor/dev/config/SpringConfig.java
@@ -86,13 +86,11 @@ public class SpringConfig implements WebMvcConfigurer {
     private Properties hibernateProperties() {
         Properties properties = new Properties();
         properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
 
         return properties;
     }
 
-    // Hibernate and JDBCTemplate
 
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
